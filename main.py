dac96e8579e92c82c584c0b5ce92c6582711bdb4
244798Ania <244798@student.pwr.edu.pl>
2021-12-09 11:52:12 +0100

    poczatek kodu do swiecenia dioda

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 # tryb numeracji pinow
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.IN)
 
+#echo none > $led0Folder/trigger
+
 while True:
-    print(GPIO.input(11))
+    if(GPIO.input(11)):
+        #zapal diode
+        #echo 1 >/sys/class/leds/led0/brightness
+        #echo 0 >/sys/class/leds/led0/brightness
+
     time.sleep(0.5)
 
-#komentarz
+
